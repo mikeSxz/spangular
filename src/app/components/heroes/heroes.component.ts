762ed3f1e6c39b9d2eb4f31332f124412c0c8f2c
msuarez <miguel.suarez.ext@nokia.com>
2019-01-13 00:20:25 -0600

    Cambiar de ruta con parametro usando Router desde el componente

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HeroesService, Heroe } from "../../services/heroes.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-heroes",
@@ -8,11 +9,16 @@ import { HeroesService, Heroe } from "../../services/heroes.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
-  constructor(private _heroesService: HeroesService) {}
+  constructor(private _heroesService: HeroesService,
+              private router: Router) {}
 
   ngOnInit() {
     console.log("ngOnInit");
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
+  }
+
+  verHeroe(idx: number) {
+    this.router.navigate(["/heroe", idx]);
   }
 }
